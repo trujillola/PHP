af6ab4b002ffbf39efc1f8da4ae89966f9e55f01
trujillola <60228294+trujillola@users.noreply.github.com>
2020-03-19 19:57:50 +0100

    Add files via upload

---
+++ test.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Ma page web</title>
+</head>
+<body>
+	<h1>Ma page web</h1>
+	<p>Bonjour.....</p>
+    <ul>
+        <li style="color: blue;">Texte en bleu</li>
+        <li style="color: red;">Texte en rouge</li>
+        <li style="color: green;">Texte en vert</li>
+    </ul>
+    <?php
+       echo "Ceci est du texte";
+       $age_du_visiteur = 17;
+    ?>
+</body>
+</html>
